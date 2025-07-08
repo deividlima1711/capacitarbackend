af3af44c0198b8e82a67bd74e5a3a4d6adf39068
deividlima1711 <limadeivid1711@gmail.com>
2025-07-08 11:01:01 -0300

    Update ensureAdminUser.js

--- src/models/ensureAdminUser.js
+++ src/models/ensureAdminUser.js
@@ -1,22 +1,19 @@
-const bcrypt = require('bcryptjs');
 const User = require('./User');
 
 async function ensureAdminUser() {
   const adminUsername = 'admin';
   const adminPassword = 'Lima12345';
 
-  const passwordHash = await bcrypt.hash(adminPassword, 10);
-
   let admin = await User.findOne({ username: adminUsername });
 
   if (admin) {
-    admin.password = passwordHash;
+    admin.password = adminPassword; // senha em texto puro, middleware fará o hash
     await admin.save();
     console.log('Senha do admin atualizada.');
   } else {
     await User.create({
       username: adminUsername,
-      password: passwordHash,
+      password: adminPassword, // senha em texto puro, middleware fará o hash
       role: 'admin',
       name: 'Administrador',
       email: 'admin@processflow.com',
